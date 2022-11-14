d2d6abd1c97baf19d78dbe3cbb513f5b49d3d4b5
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-11-14 11:05:57 +0300

    Examples005_Else

--- Examples005_Else/Program.cs
+++ Examples005_Else/Program.cs
@@ -2,7 +2,7 @@
 System.Console.WriteLine("Введите имя");
 string UserName = Console.ReadLine();
 
-if(UserName == "Андрей"){
+if(UserName.ToLower()== "андрей"){
     System.Console.WriteLine("Где правки?");
 } else{
     System.Console.WriteLine("привет " + UserName);
