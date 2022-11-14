5ff5b2ccbb7e7b4da9ea5c043e2848e582926c09
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-11-14 11:07:28 +0300

    Examples009_introArray

---
+++ Examples009_introArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int Max(int arg1, int arg2,int arg3){
+    int result = arg1;
+    if(arg2 >result) result = arg2;
+    if(arg3 >result) result= arg3;
+    return result;
+}
+int[] array = {11,12,23,44,58,169,70,80,9};
+array[0] = 12;
+
+
+int result = Max(
+    Max(array[0],array[1],array[2]),
+    Max(array[3],array[4],array[5]),
+    Max(array[6],array[7],array[8]));
+    System.Console.WriteLine(result);
+
+
