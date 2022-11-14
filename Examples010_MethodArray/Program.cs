2413831ce7a776ebf076a82daffee91ec55fdbb4
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-11-14 11:07:50 +0300

    Examples010_MethodArray

---
+++ Examples010_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {81,3,3,24,35,16,7,88};
+
+int n = array.Length;
+int find = 3;
+int index = 0;
+while(index<n){
+    if(array[index]==find){
+        Console.WriteLine($"Номер в массиве: " + index);
+        break;
+    }
+    index++;
+}
+
